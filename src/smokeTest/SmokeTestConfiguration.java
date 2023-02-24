edf43b7435703aa9c07da2ae851ca4c48b82af40
Scaffolder <scaffolder@backstage.io>
2023-02-24 08:42:19 +0000

    initial commit

---
+++ src/smokeTest/SmokeTestConfiguration.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.labs.Sabirv1.smoketests;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@ComponentScan("uk.gov.hmcts.labs-Sabir-v1.smoketests")
+@PropertySource("application.properties")
+public class SmokeTestConfiguration {
+  //TODO: implement smoke tests here
+}
